5a72a56ee4b25da9b25f05f38d94a0c68b8f313e
tv.unimol <doslovno@gmail.com>
2019-01-05 21:40:02 +0300

    MainActivity implements View

--- app/src/main/java/com/example/manifest/mvp/mainscreen/MainActivity.java
+++ app/src/main/java/com/example/manifest/mvp/mainscreen/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.manifest.mvp.mainscreen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.manifest.mvp.App;
 import com.example.manifest.mvp.R;
+import com.example.manifest.mvp.data.Post;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity implements MainScreenContract.View {
+
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
+    @Inject
+    MainScreenPresenter mainScreenPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.my_list);
+        list = new ArrayList<>();
+
+        DaggerMainScreenComponent.builder()
+                .netComponent(((App) getApplication()).getNetComponent())
+                .mainScreenModule(new MainScreenModule(this))
+                .build()
+                .inject(this);
+
+        mainScreenPresenter.loadPost();
+    }
+
+    @Override
+    public void showPosts(List<Post> posts) {
+        for (Post each : posts) {
+            list.add(each.getTitle());
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(getApplicationContext(), "Error" + message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showComplete() {
+        Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_SHORT).show();
     }
 }
