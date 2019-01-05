63d221c844eda8c675e4973fb03cbe44ce1e7b5f
tv.unimol <doslovno@gmail.com>
2019-01-05 17:08:09 +0300

    add AppModule, NetModule

---
+++ app/src/main/java/com/example/manifest/mvp/data/module/NetModule.java
@@ -0,0 +1,65 @@
+package com.example.manifest.mvp.data.module;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class NetModule {
+
+    String baseUrl;
+
+    @Provides
+    @Singleton
+    SharedPreferences providesSharedPreference(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+    @Provides
+    @Singleton
+    Cache provideHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024;
+        return new Cache(application.getCacheDir(), cacheSize);
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return builder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .cache(cache);
+        return builder.build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient client) {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(baseUrl)
+                .client(client);
+        return builder.build();
+    }
+}
