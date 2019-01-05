fb26e17e518b2d5f4b2bb71eda084fd3c23cbd7a
tv.unimol <doslovno@gmail.com>
2019-01-05 18:20:38 +0300

    add MainScreenContract class

---
+++ app/src/main/java/com/example/manifest/mvp/mainscreen/MainScreenContract.java
@@ -0,0 +1,22 @@
+package com.example.manifest.mvp.mainscreen;
+
+import com.example.manifest.mvp.data.Post;
+
+import java.util.List;
+
+public interface MainScreenContract {
+
+    interface View {
+
+        void showPosts(List<Post> posts);
+
+        void showError(String message);
+
+        void showComplete();
+    }
+
+    interface Presenter {
+
+        void showPost();
+    }
+}
