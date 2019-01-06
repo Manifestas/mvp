c64d6c482f381dbde1ac0b2b19926b16d992a558
manifest <doslovno@gmail.com>
2019-01-06 15:38:45 +0300

    add EspressoIdling for testing

--- app/src/main/java/com/example/manifest/mvp/mainscreen/MainActivity.java
+++ app/src/main/java/com/example/manifest/mvp/mainscreen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.manifest.mvp.mainscreen;
 
 import android.os.Bundle;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 import com.example.manifest.mvp.App;
 import com.example.manifest.mvp.R;
 import com.example.manifest.mvp.data.Post;
+import com.example.manifest.mvp.util.EspressoIdlingResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements MainScreenContrac
                 .build()
                 .inject(this);
 
+        //Increment the counter before making a network request
+        EspressoIdlingResource.increment();
+
         mainScreenPresenter.loadPost();
     }
 
@@ -48,15 +54,26 @@ public class MainActivity extends AppCompatActivity implements MainScreenContrac
         }
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
+
+        //Decrement after loading the posts
+        EspressoIdlingResource.decrement();
     }
 
     @Override
     public void showError(String message) {
         Toast.makeText(getApplicationContext(), "Error" + message, Toast.LENGTH_SHORT).show();
+
+        // If there is no network connection we get an error and decrement the counter because the call has finished
+        EspressoIdlingResource.decrement();
     }
 
     @Override
     public void showComplete() {
         Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_SHORT).show();
+    }
+
+    @VisibleForTesting
+    public IdlingResource getCountingIdlingResource() {
+        return EspressoIdlingResource.getIdlingResource();
     }
 }
